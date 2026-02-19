0e31ebbe024cd59a5c5afb7ed1e3a2e63290d002
zizwanphgziz <zizwanphgziz@gmail.com>
2026-02-19 15:03:48 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+
   const targetUrl = req.query.url;
 
   if (!targetUrl) {
@@ -9,28 +10,28 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "API token not configured" });
   }
 
+  // Detect platform
+  let origin = "https://www.dramabox.com";
+
+  if (targetUrl.includes("/netshort/")) {
+    origin = "https://www.netshort.com";
+  }
+
+  if (targetUrl.includes("/reelshort/")) {
+    origin = "https://www.reelshort.com";
+  }
+
   try {
-
-    // detect platform from url
-    let origin = "https://www.dramabox.com";
-
-    if (targetUrl.includes("/netshort/")) {
-      origin = "https://www.netshort.com";
-    }
 
     const apiResponse = await fetch(targetUrl, {
       headers: {
         Authorization: `Bearer ${process.env.API_TOKEN}`,
-        Origin: origin
+        Origin: origin,
+        Referer: origin
       }
     });
 
     const contentType = apiResponse.headers.get("content-type");
-
-    if (!apiResponse.ok) {
-      const errText = await apiResponse.text();
-      return res.status(apiResponse.status).send(errText);
-    }
 
     if (contentType && contentType.includes("application/json")) {
       const data = await apiResponse.json();
